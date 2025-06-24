6fdf5ae4d0833e22b4bb4f7f3cda9cc8888864df
wildandr <wildandzakyramadhani@mail.com>
2025-06-24 22:06:41 +0700

    update

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+         return (
+           <div>
+             <h1>Welcome to the Home Page</h1>
+             <p>This is a test page for Umami analytics.</p>
+             <a href="/about">Go to About Page</a>
+           </div>
+         );
+       }
